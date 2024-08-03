955901ad698c7d0b65b95b71ce91ef0b3b625708
DomTorres <neildominictorres@gmail.com>
2024-08-03 02:42:32 -0700

    first commit

---
+++ backend/src/test/java/domtorres/harmonize/HarmonizeApplicationTests.java
@@ -0,0 +1,13 @@
+package domtorres.harmonize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarmonizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
